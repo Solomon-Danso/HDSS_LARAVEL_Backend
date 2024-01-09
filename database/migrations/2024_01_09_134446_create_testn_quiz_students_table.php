dfa30f801232938db5753036610a338357f8cebf
Solomon <solomondanso2023@gmail.com>
2024-01-09 13:52:32 +0000

    Models

---
+++ database/migrations/2024_01_09_134446_create_testn_quiz_students_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testn_quiz_students', function (Blueprint $table) {
+            $table->id();
+            $table->longText("QuizId")->nullable();
+            $table->longText("Subject")->nullable();
+            $table->longText("Level")->nullable();
+            $table->longText("Question")->nullable();
+            $table->longText("OptionA")->nullable();
+            $table->longText("OptionB")->nullable();
+            $table->longText("OptionC")->nullable();
+            $table->longText("OptionD")->nullable();
+            $table->longText("OptionE")->nullable();
+            $table->longText("Answer")->nullable();
+            $table->boolean("IsAnswered")->default(false);
+            $table->boolean("IsStarted")->default(false);
+            $table->dateTime("Deadline")->nullable();
+            $table->integer("Duration")->nullable();
+           
+            $table->longText("StudentId")->nullable();
+            $table->longText("StudentName")->nullable();
+            $table->longText("ProfilePic")->nullable();
+            $table->float("DesignatedMarks")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testn_quiz_students');
+    }
+};
