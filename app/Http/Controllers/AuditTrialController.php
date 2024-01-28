b8cdb9b8a103ff091614d91ee43e261782f2aca8
Solomon <solomondanso2023@gmail.com>
2024-01-28 20:41:41 +0000

    Mature way of coding

--- app/Http/Controllers/AuditTrialController.php
+++ app/Http/Controllers/AuditTrialController.php
@@ -3,8 +3,226 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\Admin;
+use App\Models\AuditTrail;
+use Carbon\Carbon;
+use App\Models\CompanyToken;
 
 class AuditTrialController extends Controller
 {
-    //
+   
+    function StudentAudit($StudentId,$CompanyId, $Action) {
+        $ipAddress = $_SERVER['REMOTE_ADDR']; // Get user's IP address
+    
+        $ipDetails = json_decode(file_get_contents("https://ipinfo.io/{$ipAddress}/json"));
+    
+        $country = $ipDetails->country ?? 'Unknown';
+        $city = $ipDetails->city ?? 'Unknown';
+    
+        // Get user agent information
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    
+        // Parse the user agent string to determine device and OS
+        $device = $this->detectDevice($userAgent);
+        $os =  $this->detectOperatingSystem($userAgent);
+    
+        // Current date and time
+     
+    
+        // URL path
+        $urlPath = $_SERVER['REQUEST_URI'];
+
+        $stu = Student::where('StudentId', $StudentId)->where("CompanyId", $CompanyId)->first();
+        if($stu==null){
+            return response()->json(["message"=>"Student does not exist"],400);
+        }
+    
+       
+        $latitude = $ipDetails->loc ?? ''; // Latitude
+        $googleMapsLink = "https://maps.google.com/?q={$latitude}";
+    
+        // Create a new AuditTrail instance and save the log to the database
+        $auditTrail = new AuditTrail();
+        $auditTrail->ipAddress = $ipAddress??" ";
+        $auditTrail->country = $country??" ";
+        $auditTrail->city = $city??" ";
+        $auditTrail->device = $device??" ";
+        $auditTrail->os = $os??" ";
+        $auditTrail->urlPath = $urlPath??" ";
+        $auditTrail->action = $Action??" "; 
+        $auditTrail->googlemap = $googleMapsLink??" ";
+        $auditTrail -> userId = $stu->StudentId??" ";
+        $auditTrail -> userName = $stu->FirstName." ".$stu->OtherName." ".$stu->LastName??" ";
+        $auditTrail -> userPic = $stu->ProfilePic??" ";
+        $auditTrail -> companyId = $stu->CompanyId??" ";
+
+        
+        $auditTrail->save();
+    }
+
+    function TeacherAudit($TeacherId,$CompanyId,$Action) {
+        $ipAddress = $_SERVER['REMOTE_ADDR']; // Get user's IP address
+    
+        $ipDetails = json_decode(file_get_contents("https://ipinfo.io/{$ipAddress}/json"));
+    
+        $country = $ipDetails->country ?? 'Unknown';
+        $city = $ipDetails->city ?? 'Unknown';
+    
+        // Get user agent information
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    
+        // Parse the user agent string to determine device and OS
+        $device = $this->detectDevice($userAgent);
+        $os =  $this->detectOperatingSystem($userAgent);
+    
+        $stu = Teacher::where('TeacherId', $TeacherId)->where("CompanyId", $CompanyId)->first();
+        if($stu==null){
+            return response()->json(["message"=>"Teacher does not exist"],400);
+        }
+      
+    
+        // URL path
+        $urlPath = $_SERVER['REQUEST_URI'];
+    
+       
+        $latitude = $ipDetails->loc ?? ''; // Latitude
+        $googleMapsLink = "https://maps.google.com/?q={$latitude}";
+    
+        // Create a new AuditTrail instance and save the log to the database
+        $auditTrail = new AuditTrail();
+        $auditTrail->ipAddress = $ipAddress??" ";
+        $auditTrail->country = $country??" ";
+        $auditTrail->city = $city??" ";
+        $auditTrail->device = $device??" ";
+        $auditTrail->os = $os??" ";
+        $auditTrail->urlPath = $urlPath??" ";
+        $auditTrail->action = $Action??" "; 
+        $auditTrail->googlemap = $googleMapsLink??" ";
+        $auditTrail -> userId = $stu->TeacherId??" ";
+        $auditTrail -> userName = $stu->FirstName." ".$stu->OtherName." ".$stu->LastName??" ";
+        $auditTrail -> userPic = $stu->ProfilePic??" ";
+        $auditTrail -> companyId = $stu->CompanyId??" ";
+        
+        $auditTrail->save();
+    }
+
+    function AdminAudit($AdminId,$CompanyId,$Action) {
+        $ipAddress = $_SERVER['REMOTE_ADDR']; // Get user's IP address
+    
+        $ipDetails = json_decode(file_get_contents("https://ipinfo.io/{$ipAddress}/json"));
+    
+        $country = $ipDetails->country ?? 'Unknown';
+        $city = $ipDetails->city ?? 'Unknown';
+    
+        // Get user agent information
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    
+        // Parse the user agent string to determine device and OS
+        $device = $this->detectDevice($userAgent);
+        $os =  $this->detectOperatingSystem($userAgent);
+    
+        $stu = Admin::where('AdminId', $AdminId)->where("CompanyId", $CompanyId)->first();
+        if($stu==null){
+            return response()->json(["message"=>"Admin does not exist"],400);
+        }
+    
+        // URL path
+        $urlPath = $_SERVER['REQUEST_URI'];
+    
+       
+        $latitude = $ipDetails->loc ?? ''; // Latitude
+        $googleMapsLink = "https://maps.google.com/?q={$latitude}";
+    
+        // Create a new AuditTrail instance and save the log to the database
+       
+        $auditTrail = new AuditTrail();
+        $auditTrail->ipAddress = $ipAddress??" ";
+        $auditTrail->country = $country??" ";
+        $auditTrail->city = $city??" ";
+        $auditTrail->device = $device??" ";
+        $auditTrail->os = $os??" ";
+        $auditTrail->urlPath = $urlPath??" ";
+        $auditTrail->action = $Action??" "; 
+        $auditTrail->googlemap = $googleMapsLink??" ";
+        $auditTrail -> userId = $stu->AdminId??" ";
+        $auditTrail -> userName = $stu->FirstName." ".$stu->OtherName." ".$stu->LastName??" ";
+        $auditTrail -> userPic = $stu->ProfilePic??" ";
+        $auditTrail -> companyId = $stu->CompanyId??" ";
+
+        $auditTrail->save();
+    }
+
+
+    function PrepaidMeter($CompanyId){
+        $c = CompanyToken::where('CompanyId', $CompanyId)->latest()->first();
+
+        if(!$c){
+            return response()->json(["message"=>"No products subscribed"],400);
+        }
+
+        $systemDate = Carbon::now();
+
+        if($systemDate < $c->CurrentDate){
+            return response()->json(["message"=>"Verify that your server is configured with the accurate date and time settings."],400);
+        }
+        else{
+            $c->CurrentDate = $systemDate;
+            $c->save();
+        }
+
+        if($systemDate > $c->ExpireDate){
+            return response()->json(["message"=>"Your subscription has expired. To continue enjoying our service, please renew your subscription. For further details, please reach out to business@hydottech.com."],400);
+
+        }
+
+
+
+
+
+
+
+    }
+
+    function IdGenerator(): string {
+        $randomID = str_pad(mt_rand(1, 99999999), 8, '0', STR_PAD_LEFT);
+        return $randomID;
+        }
+
+
+
+
+    
+    // Function to detect device type from User-Agent string
+    function detectDevice($userAgent) {
+        $isMobile = false;
+        $mobileKeywords = ['Android', 'webOS', 'iPhone', 'iPad', 'iPod', 'BlackBerry', 'Windows Phone'];
+    
+        foreach ($mobileKeywords as $keyword) {
+            if (stripos($userAgent, $keyword) !== false) {
+                $isMobile = true;
+                break;
+            }
+        }
+    
+        return $isMobile ? 'Mobile' : 'Desktop';
+    }
+    
+    // Function to detect operating system from User-Agent string
+    function detectOperatingSystem($userAgent) {
+        $os = 'Unknown';
+    
+        $osKeywords = ['Windows', 'Linux', 'Macintosh', 'iOS', 'Android'];
+    
+        foreach ($osKeywords as $keyword) {
+            if (stripos($userAgent, $keyword) !== false) {
+                $os = $keyword;
+                break;
+            }
+        }
+    
+        return $os;
+    }
+    
 }
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
-use App\Models\CompanyToken;
-use Carbon\Carbon;
+
+use App\Http\Controllers\AuditTrialController;
+
 use App\Jobs\ProcessBulkStudentRegistration;
 
 
 class StudentController extends Controller
 {
+
+    protected $audit;
+
+    public function __construct(AuditTrialController $auditTrialController)
+    {
+        $this->audit = $auditTrialController;
+    }
+
+
     function RegisterStudent(Request $req){
 
-        $response = $this->PrepaidMeter($req->CompanyId);
+        $response = $this->audit->PrepaidMeter($req->CompanyId);
 
         if ($response !== null && $response->getStatusCode() !== 200) {
             return $response;
@@ -28,7 +38,7 @@ class StudentController extends Controller
         $s->save();
 
 
-$s->StudentId = strval(10000 + $s->id);
+    $s->StudentId = strval(10000 + $s->id);
 
 
         
@@ -174,7 +184,7 @@ $s->StudentId = strval(10000 + $s->id);
        $saver = $s->save();
 
        if($saver){
-
+        $this->audit->StudentAudit($s->StudentId, $s->CompanyId, "Registered A Student");
         return response()->json(["message" => "Student Admitted Successfully"],200);
        }
        else{
@@ -189,7 +199,7 @@ $s->StudentId = strval(10000 + $s->id);
 
     public function BulkRegisterStudent(Request $req)
     {
-        $response = $this->PrepaidMeter($req->CompanyId);
+        $response = $this->audit->PrepaidMeter($req->CompanyId);
     
         if ($response !== null && $response->getStatusCode() !== 200) {
             return $response;
@@ -210,47 +220,212 @@ $s->StudentId = strval(10000 + $s->id);
         return response()->json(["message" => "No file uploaded."], 400);
     }
     
+    function UpdateStudent(Request $req){
 
+        $response = $this->audit->PrepaidMeter($req->CompanyId);
 
-
-
-
-
-
-
-
-    function PrepaidMeter($CompanyId){
-        $c = CompanyToken::where('CompanyId', $CompanyId)->latest()->first();
-
-        if(!$c){
-            return response()->json(["message"=>"No products subscribed"],400);
+        if ($response !== null && $response->getStatusCode() !== 200) {
+            return $response;
         }
 
-        $systemDate = Carbon::now();
-
-        if($systemDate < $c->CurrentDate){
-            return response()->json(["message"=>"Verify that your server is configured with the accurate date and time settings."],400);
-        }
-        else{
-            $c->CurrentDate = $systemDate;
-            $c->save();
+        $s = Student::where('StudentId', $req->StudentId)->where("CompanyId", $req->CompanyId)->first();
+        if($s==null){
+            return response()->json(["message"=>"Student does not exist"],400);
         }
 
-        if($systemDate > $c->ExpireDate){
-            return response()->json(["message"=>"Your subscription has expired. To continue enjoying our service, please renew your subscription. For further details, please reach out to business@hydottech.com."],400);
+        if($req->hasFile("ProfilePic")){
+            $s->ProfilePic = $req->file("ProfilePic")->store("","public");
+        }  
 
+
+        if($req->filled("FirstName")){
+            $s->FirstName  = $req ->FirstName  ;
         }
 
+        if($req->filled("OtherName")){
+            $s->OtherName  = $req ->OtherName  ;
+        }
 
+        if($req->filled("LastName")){
+            $s->LastName  = $req ->LastName  ;
+        }
 
+        if($req->filled("DateOfBirth")){
+            $s->DateOfBirth  = $req ->DateOfBirth  ;
+        }
 
+        if($req->filled("Gender")){
+            $s->Gender  = $req ->Gender  ;
+        }
 
+        if($req->filled("HomeTown")){
+            $s->HomeTown  = $req ->HomeTown  ;
+        }
+
+        if($req->filled("Location")){
+            $s->Location  = $req ->Location  ;
+        }
+
+        if($req->filled("Country")){
+            $s->Country  = $req ->Country  ;
+        }
+
+        if($req->filled("FathersName")){
+            $s->FathersName  = $req ->FathersName  ;
+        }
+
+        if($req->filled("FatherOccupation")){
+            $s->FatherOccupation  = $req ->FatherOccupation  ;
+        }
+
+        if($req->filled("MothersName")){
+            $s->MothersName  = $req ->MothersName  ;
+        }
+
+        if($req->filled("MotherOccupation")){
+            $s->MotherOccupation  = $req ->MotherOccupation  ;
+        }
+
+        if($req->filled("GuardianName")){
+            $s->GuardianName  = $req ->GuardianName  ;
+        }
+
+        if($req->filled("GuardianOccupation")){
+            $s->GuardianOccupation  = $req ->GuardianOccupation  ;
+        }
+
+        if($req->filled("ParentLocation")){
+            $s->ParentLocation  = $req ->ParentLocation  ;
+        }
+
+        if($req->filled("ParentDigitalAddress")){
+            $s->ParentDigitalAddress  = $req ->ParentDigitalAddress  ;
+        }
+
+        if($req->filled("ParentReligion")){
+            $s->ParentReligion  = $req ->ParentReligion  ;
+        }
+
+        if($req->filled("ParentEmail")){
+            $s->ParentEmail  = $req ->ParentEmail  ;
+        }
+
+        if($req->filled("EmergencyContactName")){
+            $s->EmergencyContactName  = $req ->EmergencyContactName  ;
+        }
+
+        if($req->filled("EmergencyPhoneNumber")){
+            $s->EmergencyPhoneNumber  = $req ->EmergencyPhoneNumber  ;
+        }
+
+        if($req->filled("EmergencyAlternatePhoneNumber")){
+            $s->EmergencyAlternatePhoneNumber  = $req ->EmergencyAlternatePhoneNumber  ;
+        }
+
+        if($req->filled("RelationshipWithChild")){
+            $s->RelationshipWithChild = $req->RelationshipWithChild  ;
+        }
+
+        if($req->filled("ParentPhoneNumber")){
+            $s->ParentPhoneNumber = $req->ParentPhoneNumber  ;
+        }
+
+        if($req->filled("Religion")){
+            $s->Religion = $req->Religion  ;
+        }
+
+        if($req->filled("Email")){
+            $s->Email = $req->Email  ;
+        }
+
+        if($req->filled("PhoneNumber")){
+            $s->PhoneNumber = $req->PhoneNumber  ;
+        }
+
+        if($req->filled("AlternatePhoneNumber")){
+            $s->AlternatePhoneNumber = $req->AlternatePhoneNumber  ;
+        }
+
+        if($req->filled("MedicalIInformation")){
+            $s->MedicalIInformation = $req->MedicalIInformation  ;
+        }
+
+        if($req->filled("Level")){
+            $s-> Level= $req-> Level ;
+        }
+
+        if($req->filled("AdmissionDate")){
+            $s->AdmissionDate = $req->AdmissionDate  ;
+        }
+
+        if($req->filled("TheAcademicYear")){
+            $s->TheAcademicYear = $req->TheAcademicYear  ;
+        }
+
+        if($req->filled("TheAcademicTerm")){
+            $s->TheAcademicTerm = $req->TheAcademicTerm  ;
+        }
+
+       
+            $s->Role = "Student" ;
+        
+
+       $saver = $s->save();
+
+       if($saver){
+
+        return response()->json(["message" => "Student Updated Successfully"],200);
+       }
+       else{
+        return response()->json(["message" => "Student Update Failed"],400);
+
+       }
+       
 
 
     }
 
-    function IdGenerator(): string {
-        $randomID = str_pad(mt_rand(1, 99999999), 8, '0', STR_PAD_LEFT);
-        return $randomID;
+    function GetStudent($StudentId, $CompanyId){
+        $response = $this->audit->PrepaidMeter($CompanyId);
+
+        if ($response !== null && $response->getStatusCode() !== 200) {
+            return $response;
         }
+
+        $s = Student::where('StudentId', $StudentId)->where("CompanyId", $CompanyId)->first();
+        if($s==null){
+            return response()->json(["message"=>"Student does not exist"],400);
+        }
+
+        return $s;
+    }
+
+    function DeleteStudent($StudentId, $CompanyId){
+        $response = $this->audit->PrepaidMeter($CompanyId);
+
+        if ($response !== null && $response->getStatusCode() !== 200) {
+            return $response;
+        }
+       
+        $s = Student::where('StudentId', $StudentId)->where("CompanyId", $CompanyId)->first();
+        if($s==null){
+            return response()->json(["message"=>"Student does not exist"],400);
+        }
+        $saver = $s->delete();
+
+        return $s;
+    }
+
+
+   
+
+
+
+
+
+
+
+
+
+
 }
